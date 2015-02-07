460ff00fda017512185abdac9c2a50cb46bc819d
Jin <jinoo@lumoslabs.com>
2015-02-07 14:33:09 -0800

    Initial Commit

---
+++ media-support/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  media-support
+//
+//  Created by Jinwoo Baek on 2/7/15.
+//  Copyright (c) 2015 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ media-support/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  media-support
+//
+//  Created by Jinwoo Baek on 2/7/15.
+//  Copyright (c) 2015 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
